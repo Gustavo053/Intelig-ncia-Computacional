4094e4f48b2186afc5310e1912a1953c89474e2b
Gustavo Pereira da Fonseca <39752736+Gustavo053@users.noreply.github.com>
2020-03-11 12:04:39 -0300

    Funções principais em andamento

---
+++ Labirinto/src/labirinto/Labirinto.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labirinto;
+import javafx.util.Pair;
+import java.util.LinkedList;
+/**
+ *
+ * @author Gustavo
+ */
+public class Labirinto {
+    int[][] arr;
+    
+    private boolean invalid(Pair<Integer, Integer> par){
+        if(par.getKey() < 0 || par.getValue() < 0){
+            return false;
+        }else if(arr[par.getKey()][par.getValue()] == '#'){
+            return false;
+        }else{
+            return true;
+        }     
+    }
+    
+    private void buscaEmProfundidade(Pair<Integer, Integer> ini, Pair<Integer, Integer> end){
+        if(invalid(ini)){
+            return;
+        }else if(ini == end){
+            System.out.println("Achei!");
+            return;
+        }else{
+            buscaEmProfundidade( new Pair(ini.getKey()-1, ini.getValue()), end);
+            buscaEmProfundidade(new Pair(ini.getKey(), ini.getValue()+1), end);
+            buscaEmProfundidade(new Pair(ini.getKey()+1, ini.getValue()), end);
+            buscaEmProfundidade(new Pair(ini.getKey(), ini.getValue()-1), end);
+        }
+    }
+    
+    private void buscaEmLargura(Pair<Integer, Integer> ini, Pair<Integer, Integer> end){
+        LinkedList fila = new LinkedList();
+        Pair<Integer, Integer> current;
+        fila.add(ini);
+        //Continuação
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
